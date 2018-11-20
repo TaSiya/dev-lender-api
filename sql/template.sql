a9e8d9d796d9af83539bdf67b236b0d47ddd256e
tasiya <siyandamakhalima.1@gmail.com>
2018-11-20 17:55:36 +0200

    initial commit

---
+++ sql/template.sql
@@ -0,0 +1,14 @@
+DROP_EXISTING 
+
+create table info(
+    id serial primary key not null,
+    name text not null,
+    age int not null,
+    bio text not null,
+    picture text ,
+    address text not null,
+    contact text not null,
+    place text not null,
+    dev_id int not null,
+    lender_id int not null
+);
